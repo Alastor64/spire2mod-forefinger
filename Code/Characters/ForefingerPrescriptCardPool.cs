7148ded4b409bfc387bbf525db7f696d76e67051
unknown <yihanhuang.bill@outlook.com>
2026-08-31 16:03:27 +0800

    注册指令池为共享卡池，修复下回合执行生成卡牌节点时落入MockCardPool抛异常

--- Code/Characters/ForefingerPrescriptCardPool.cs
+++ Code/Characters/ForefingerPrescriptCardPool.cs
@@ -1,9 +1,13 @@
 using Godot;
+using STS2RitsuLib.Interop.AutoRegistration;
 using STS2RitsuLib.Scaffolding.Content;
 
 namespace Forefinger.Characters;
 
 // 指令池：与角色池、奖励池分开的卡牌池，卡牌总览中暂用无色图标。
+// 必须注册为共享卡池，否则卡牌池解析（CardModel.Pool）会落到测试用的 MockCardPool
+// 并在生成卡牌节点时抛异常，导致「下回合执行」无法把指令牌加入手牌。
+[RegisterSharedCardPool]
 public sealed class ForefingerPrescriptCardPool : TypeListCardPoolModel
 {
     public override string Title => "指令";
